17e25cde16c8033aee252ed7c3827b693561c253
Marsh1894 <dawid.patola1@gmail.com>
2022-05-16 21:08:40 +0200

    make it better

--- main.py
+++ main.py
@@ -44,12 +44,14 @@ def get_day(name: str, number: int, response: Response):
 
 
 @app.put('/events', status_code=200)
-def get_date(in_json):
+def get_date(in_json, response: Response):
     date_added = datetime.date(datetime.now())
-    json_2 = {
+    out_json = {
         "id" : 0,
         "name": in_json['event'],
         "date": in_json['date'],
         "date_added": date_added
     }
+    response.status_code = status.HTTP_200_OK
+    return{out_json}
 
