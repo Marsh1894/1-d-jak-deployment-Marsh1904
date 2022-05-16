aeed8676f82674d57ffade0c0027fd64cebb95df
Marsh1894 <dawid.patola1@gmail.com>
2022-05-16 18:48:14 +0200

    make it better

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from typing import Dict
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"start": "1970-01-01"}
+
+@app.get("/method")
+def root():
+    return {"method": "1970-01-01"}
