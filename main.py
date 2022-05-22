e7c89e1f31ca8e0490b8c544fd25bda6544ee3ee
Marsh1894 <dawid.patola1@gmail.com>
2022-05-22 17:00:15 +0200

    make it better

--- main.py
+++ main.py
@@ -144,79 +144,133 @@
 
 
 import datetime
-from fastapi import FastAPI, Response, status
-from pydantic import BaseModel
+from typing import List
+
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel, BaseSettings
+
+
+class Settings(BaseSettings):
+    events_counter: int = 0
+
+
+class EventCounterRq(BaseModel):
+    event: str
+    date: str
+
+
+class EventCounterResponse(BaseModel):
+    name: str
+    date: str
+    id: int
+    date_added: str
 
 
 app = FastAPI()
+settings = Settings()
+
+events: List[EventCounterResponse] = []
+
+
 @app.get("/")
 def root():
-    return {'start': '1970-01-01'}
+    return {"start": "1970-01-01"}
 
-@app.post("/method", status_code=201)
-def method_post():
+
+@app.post(path="/method", status_code=201)
+def get_post():
     return {"method": "POST"}
 
-@app.get("/method")
-def method_get():
-    return {"method": "GET"}
 
-@app.put("/method")
-def method_put():
-    return {"method": "PUT"}
-
-@app.options("/method")
-def method_options():
-    return {"method": "OPTIONS"}
-
-@app.delete("/method")
-def method_delete():
-    return {"method": "DELETE"}
-
-days = {1:"monday", 2:"tuesday", 3:"wednesday", 4:"thursday", 5:"friday", 6:"saturday", 7:"sunday"}
+@app.api_route(
+    path="/method",
+    methods=["GET", "PUT", "OPTIONS", "DELETE"],
+    status_code=200,
+)
+async def get_methods(request: Request):
+    return {"method": request.method}
 
 
-@app.get("/day", status_code=200)
-def get_day(name: str, number: int, response: Response):
-        if days.get(number) == name:
-                return days[number] 
+days = {
+    1: "monday",
+    2: "tuesday",
+    3: "wednesday",
+    4: "thursday",
+    5: "friday",
+    6: "saturday",
+    7: "sunday",
+}
+
+
+@app.get("/day/", status_code=200)
+def get_day(name: str, number: int):
+    if number in days:
+        if days.get(number, False) == name:
+            return days[number]
         else:
-            response.status_code = 400
-            return {"Day or number": "not viable"}
-
-class Event(BaseModel):
-	date: str
-	event: str
-
-events = []
-
-@app.put("/events", status_code = 201)
-def put_event(event: Event, response: Response):
-    out = {
-        "id": len(events),
-        "date": event.date,
-        "name": event.event,
-        "date_added": str(datetime.date.today())
-    }
-    events.append(out)
-
-    return out
+            raise HTTPException(status_code=400, detail="Invalid day!")
+    else:
+        raise HTTPException(status_code=400, detail="Number higher than 7!")
 
 
-@app.get("/events/{date}", status_code=201)
-def get_event(date: str, response: Response):
-    event_date=[]
-    format = "/%Y-%m-%d"
+@app.put("/events", status_code=200, response_model=EventCounterResponse)
+def put_event(data: EventCounterRq):
+
+    name = data.event
+    date = data.date
+    id = settings.events_counter
+    settings.events_counter += 1
+    date_added = str(datetime.date.today())
+
+    res = EventCounterResponse(
+        name=name, date=date, id=id, date_added=date_added
+    )
+    events.append(res)
+
+    return res
+
+
+@app.get(
+    "/events/{date}",
+    status_code=200,
+    response_model=List[EventCounterResponse],
+)
+def get_event(date: str):
+
     try:
-        datetime.datetime.strptime(date, format)
-        for event in events:
-            if event["date"] == date:
-                event_date.append(event)
-        if len(event_date) == 0:
-            response.status_code = 404
-            return "not found"
-        return event_date
+        _ = (datetime.datetime.strptime(date, "%Y-%m-%d"),)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid date format")
 
-    except ValueError:
-        response.status_code = 400
-        return "Enter correct date"
+    final_events: List[EventCounterResponse] = []
+
+    for event in events:
+        if event.date == date:
+            final_events.append(event)
+
+    if len(final_events) > 0:
+        return final_events
+    else:
+        raise HTTPException(status_code=404, detail="Didn't find any data")
+
+
+# @app.get("/hello/{name}", response_model=HelloResp)
+# def read_item(name: str):
+#     return HelloResp(msg=f"Hello {name}")
+
+
+# class GiveMeSomethingRq(BaseModel):
+#     first_key: str
+
+
+# class GiveMeSomethingResp(BaseModel):
+#     received: Dict
+#     constant_data: str = "python jest super"
+
+
+# @app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+# def receive_something(rq: GiveMeSomethingRq):
+#     return GiveMeSomethingResp(received=rq.dict())
+
+# class HelloResp(BaseModel):
+#     msg: str
